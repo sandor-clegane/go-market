b8bfd355cb0f6b17a5b5acb7e7f2e234c2bc66bf
Vadim <tarelov.vad@yandex.ru>
2023-02-18 23:34:51 +0300

    add hashing passowrds

--- internal/service/userservice/userService.go
+++ internal/service/userservice/userService.go
@@ -2,7 +2,8 @@ package userservice
 
 import (
 	"context"
-	"encoding/base64"
+	"crypto/md5"
+	"io"
 
 	"github.com/sandor-clegane/go-market/internal/entities"
 	"github.com/sandor-clegane/go-market/internal/entities/customerrors"
@@ -14,21 +15,25 @@ type userServiceImpl struct {
 }
 
 func (u userServiceImpl) Create(ctx context.Context, user entities.UserRequest, userID string) error {
-	encodedPassword := base64.StdEncoding.EncodeToString([]byte(user.Password))
+	h := md5.New()
+	io.WriteString(h, user.Password)
+	encodedPassword := string(h.Sum(nil))
 
 	return u.userStorage.InsertUser(ctx, userID, user.Login, encodedPassword)
 }
 
 func (u userServiceImpl) Login(ctx context.Context, user entities.UserRequest) (string, error) {
+	//expected hash
 	foundUser, err := u.userStorage.FindByLogin(ctx, user.Login)
 	if err != nil {
 		return "", err
 	}
-	decodedPassword, err := base64.StdEncoding.DecodeString(foundUser.Password)
-	if err != nil {
-		return "", err
-	}
-	if user.Password != string(decodedPassword) {
+	//input hash
+	h := md5.New()
+	io.WriteString(h, user.Password)
+	encodedPassword := string(h.Sum(nil))
+
+	if foundUser.Password != encodedPassword {
 		return "", customerrors.NewInvalidPasswordError(user.Password)
 	}
 
